06cf447d2672e6fcbf6ceeab695044cfa1aab8d0
Anand <anand.mnhr@gmail.com>
2022-02-06 09:07:55 +0530

    commiting changes for Patner class

---
+++ OctBatch/src/test/java/org/var/Patner.java
@@ -0,0 +1,23 @@
+package test.java.org.var;
+
+public class Patner {
+
+	 int id=030456;
+	 String Name="PrivateOrg";
+	 
+	 public void getPatnerID() {
+		// int id=456464;
+		 System.out.println("PatnerID is: " + id);
+	 }
+	 public void getPatnerName() {
+			// int id=456464;
+			 System.out.println("PatnerID is: " + id);
+			 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Patner info = new Patner();
+		info.getPatnerID();
+		
+	}
+
+}
